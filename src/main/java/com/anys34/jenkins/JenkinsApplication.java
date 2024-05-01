4393f8a641ad8d35b5b2b816a54af280a63e901a
anys34 <anyeseong34@gmail.com>
2024-05-01 19:18:11 +0900

    init

---
+++ src/main/java/com/anys34/jenkins/JenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.anys34.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/anys34/jenkins/JenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anys34.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
