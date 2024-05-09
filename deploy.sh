4b5ff5aeb2cd2f5b12c668cfdccfc053a3270649
anys34 <anyeseong34@gmail.com>
2024-05-09 16:29:51 +0900

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,17 @@
-echo "deploy test"
+docker_username="anys34"
+image_name="jenkins_study"
+container_name="springboot"
+version="latest"
+
+if [ "$(docker ps -q -f name=${container_name})" ]; then
+    docker stop ${container_name}
+    docker rm ${container_name}
+fi
+
+if [ "$(docker images -q ${docker_username}/${image_name}:${version})" ]; then
+    docker rmi ${docker_username}/${image_name}:${version}
+fi
+
+docker pull ${docker_username}/${image_name}:${version}
+
+docker run -d -p 8080:8080 --name ${container_name} ${docker_username}/${image_name}:${version}
